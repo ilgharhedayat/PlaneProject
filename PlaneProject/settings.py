81c2c839cd9683b98125cd19ffe3bb447fee6a00
ilgharhedayat <102651388+ilgharhedayat@users.noreply.github.com>
2022-05-30 11:58:07 -0700

    Update settings.py

--- PlaneProject/settings.py
+++ PlaneProject/settings.py
@@ -90,8 +90,12 @@ WSGI_APPLICATION = "PlaneProject.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb', 
+        'USER': 'myuser', 
+        'PASSWORD': '0937maa5733',
+        'HOST': 'localhost', 
+#         'PORT': '5432',
     }
 }
 
